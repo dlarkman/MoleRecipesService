1dfe6b8720d057637e01c8662074ee4187c49b95
dzl <dzl@PLSSDWRK075.Corp.Internal>
2019-06-03 17:23:49 +0100

    Changed generation strategy on id for ingredient and recipe to IDENTITY

--- src/main/java/com/example/database/Recipe.java
+++ src/main/java/com/example/database/Recipe.java
@@ -20,7 +20,7 @@ import javax.persistence.OneToMany;
 		private static final long serialVersionUID = 1L;
 
 		@Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private long id;
 	 
 	    @Column(nullable = false, unique = true)
